d6a749b2cd8d2ba34e7ce7fb715c2ea3ed6cd2b3
Prit <20dit008@charusat.edu.in>
2022-06-10 01:16:56 +0530

    message type and send to server and fetch it from firebase

--- lib/screens/comments_screen.dart
+++ lib/screens/comments_screen.dart
@@ -1,24 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter/model/user.dart';
+import 'package:instagram_flutter/provider/user_provider.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:instagram_flutter/widgets/comment_cart.dart';
+import '../resources/firestore_methods.dart';
+import 'package:provider/provider.dart';
 
 class CommentsScreen extends StatefulWidget {
-  CommentsScreen({Key? key}) : super(key: key);
+  final snap;
+  CommentsScreen({Key? key, required this.snap}) : super(key: key);
 
   @override
   State<CommentsScreen> createState() => _CommentsScreenState();
 }
 
 class _CommentsScreenState extends State<CommentsScreen> {
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _messageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         title: const Text('Comments'),
         centerTitle: false,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('posts')
+              .doc(widget.snap['postId'])
+              .collection('comments')
+              .snapshots(),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: const CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return CommentCard( snap:(snapshot.data as dynamic).docs[index].data());
+              },
+              itemCount: (snapshot.data! as dynamic).docs.length,
+            );
+          })),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
@@ -28,26 +63,38 @@ class _CommentsScreenState extends State<CommentsScreen> {
           child: Row(
             children: [
               CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1654720110542-dddee085756f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'),
+                backgroundImage: NetworkImage(user.photoUrl),
               ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.only(left:16.0 , right: 8),
+                  padding: const EdgeInsets.only(left: 16.0, right: 8),
                   child: TextField(
+                    controller: _messageController,
                     decoration: InputDecoration(
-                      hintText: 'Comment as username',
-                      border: InputBorder.none
-                    ),
+                        hintText: user.username, border: InputBorder.none),
                   ),
                 ),
               ),
               InkWell(
-                onTap: (){},
-                child: Container(padding: const EdgeInsets.symmetric(vertical: 8 , horizontal: 8),
-                child: const Text('Post' , style: TextStyle(
-                  color: Colors.blueAccent
-                ),),
+                onTap: () async {
+                  await FireStoreMethods().postComments(
+                      _messageController.text,
+                      user.username,
+                      user.photoUrl,
+                      user.uid,
+                      widget.snap['postId']);
+
+                  setState(() {
+                    _messageController.text = "";
+                  });
+                },
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
                 ),
               )
             ],
